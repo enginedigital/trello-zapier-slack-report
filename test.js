8b2d4c1d97d50268e6a742edd2ef32e9a7447182
James Doyle <james2doyle@gmail.com>
2019-10-01 12:44:36 -0700

    remove unused code in test

--- test.js
+++ test.js
@@ -1,13 +1,7 @@
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
 
-const { URL, URLSearchParams } = require('url');
-
 // set this globally to make sure the functions get it
 global.inputData = require('./credentials.json');
-
-// polyfill the environment
-global.URL = URL;
-global.URLSearchParams = URLSearchParams;
 
 require('./index.js');
